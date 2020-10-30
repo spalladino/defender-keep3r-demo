97c22d7bc0c250a94f7f036ed342264668cc3b52
Santiago Palladino <spalladino@gmail.com>
2020-10-30 15:57:58 -0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,6 @@
 async function main() {
-  // const contract = await ethers.getContractFactory("Workable").then(f => f.deploy()).then(c => c.deployed());
-  // console.log(`Workable deployed at:\n${contract.address}`);
-
+  const contract = await ethers.getContractFactory("Workable").then(f => f.deploy()).then(c => c.deployed());
+  console.log(`Workable deployed at:\n${contract.address}`);
   const registry = await ethers.getContractFactory("KeeperRegistry").then(f => f.deploy()).then(c => c.deployed());
   console.log(`Sample keep3r registry deployed at:\n${registry.address}`);
 }
