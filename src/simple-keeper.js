f7f6914bd5b8a723d6f3451c2e92773123d5a48f
Mohamed Hayibor <11381259+mohamedhayibor@users.noreply.github.com>
2020-11-02 20:07:06 +0700

    Add instructions to run demo test (#1)
    * Add instructions to run demo test
    
    * Update readme

--- src/simple-keeper.js
+++ src/simple-keeper.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const workableAbi = [{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"worker","type":"address"}],"name":"Worked","type":"event"},{"inputs":[],"name":"requestWork","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"work","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"workable","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}];
 const workableAddr = '0xB4d258b63b1937e3442BD98Db745750E05432f37';
-const from = '0x2e51e0bd78af0e4cddb062c0bc456be0793c51af';
+const from = process.env.RINKEBY_PUBLIC_ADDR;
 
 const { ethers } = require("ethers");
 const { DefenderRelaySigner } = require('defender-relay-client/lib/ethers');
@@ -20,7 +22,6 @@ exports.handler = async function(credentials) {
 
 // To run locally (this code will not be executed in Autotasks)
 if (require.main === module) {
-  require('dotenv').config();
   const { API_KEY: apiKey, API_SECRET: apiSecret } = process.env;
   exports.handler({ apiKey, apiSecret })
     .then(() => process.exit(0))
